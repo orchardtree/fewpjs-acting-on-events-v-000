67270a871dd5a5f1dac1a6854560e1e0d220dbd5
Jesse Wenger <wengerj@gmail.com>
2021-06-01 00:37:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function moveDodgerRight() {
   let rightNumbers = dodger.style.left.replace("px", "");
   let right = parseInt(rightNumbers, 10);
 
-  if (right < 350) {
+  if (right < 360) {
     dodger.style.left = `${right + 1}px`;
   }
 }
